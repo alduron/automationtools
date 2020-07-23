413a470ae0fcc5afa86661678b83f21844d1d77f
Robert <rodell@recurvio.com>
2020-07-23 14:57:45 -0500

    Fix import bug

--- automationtools.psm1
+++ automationtools.psm1
@@ -1,15 +1,15 @@
 $Scripts = @"
-.\Conversion\Conversions.ps1
-.\Networking\Get-NetworkStatistics.ps1
-.\Logging\Logging.ps1
-.\Email\EmailHandler.ps1
-.\TaskManagement\RemoteTaskManagement.ps1
-.\MachineMaintenance\MachineMaintenance.ps1
-.\AccountMaintenance\AccountMaintenance.ps1
-.\JobManagement\JobTools.ps1
-.\Directory\FileTools.ps1
-.\Utilities\Utilities.ps1
-"@.Trim() -split "\s*[\r\n]+\s"
+	.\Conversion\Conversions.ps1
+	.\Networking\Get-NetworkStatistics.ps1
+	.\Logging\Logging.ps1
+	.\Email\EmailHandler.ps1
+	.\TaskManagement\RemoteTaskManagement.ps1
+	.\MachineMaintenance\MachineMaintenance.ps1
+	.\AccountMaintenance\AccountMaintenance.ps1
+	.\JobManagement\JobTools.ps1
+	.\Directory\FileTools.ps1
+	.\Utilities\Utilities.ps1
+"@.Trim() -split "\s*[\r\n]+\s*"
 
 Push-Location $PSScriptRoot
 Foreach($Script in $Scripts | Resolve-Path -ErrorAction Continue){
